b00ace51c2d5794bf8642f2b88bab6e48b03f02d
Akinsaya Delamarre <adelamarre@remote-learner.net>
2013-08-20 09:40:37 -0400

    MDL-41170: Added user updated event triggers to password updating functions.

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -4226,6 +4226,9 @@ function update_internal_user_password($user, $password) {
     if ($user->password !== $hashedpassword) {
         $DB->set_field('user', 'password',  $hashedpassword, array('id'=>$user->id));
         $user->password = $hashedpassword;
+
+        // Trigger user updated event
+        events_trigger('user_updated', $user);
     }
 
     return true;
@@ -5412,7 +5415,14 @@ function setnew_password_and_mail($user) {
 
     $newpassword = generate_password();
 
-    $DB->set_field('user', 'password', hash_internal_user_password($newpassword), array('id'=>$user->id));
+    $hashedpassword = hash_internal_user_password($newpassword);
+
+    $DB->set_field('user', 'password', $hashedpassword, array('id'=>$user->id));
+
+    $user->password = $hashedpassword;
+
+    // Trigger user updated event
+    events_trigger('user_updated', $user);
 
     $a = new stdClass();
     $a->firstname   = fullname($user, true);
